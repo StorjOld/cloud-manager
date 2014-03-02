d367cb965ba64ea925ba0414798a99817d3bdb04
Hugo Peixoto <hugo.peixoto@ndrive.com>
2014-03-02 02:06:36 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='cloudmanager',
+    version='0.1.0',
+    author='Hugo Peixoto',
+    author_email='hugo.peixoto@gmail.com',
+    packages=['cloudmanager'],
+    scripts=[],
+    url='https://github.com/super3/cloud-manager',
+    license='LICENSE.txt',
+    description='Manages a pool of files uploaded to "the cloud"',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    )
