ab4d0695feb905f04e8232c1cfb87ce8f358026b
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-03-09 03:57:43 +0000

    Bumps version to 0.5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cloudmanager',
-    version='0.5.1',
+    version='0.5.2',
     author='Hugo Peixoto',
     author_email='hugo.peixoto@gmail.com',
     packages=['cloudmanager'],
