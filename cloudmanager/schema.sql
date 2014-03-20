e3572034dcef3576f5367044de03808dc23492cd
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-03-20 01:40:35 +0000

    Adds month unique index to transfer meter

--- cloudmanager/schema.sql
+++ cloudmanager/schema.sql
@@ -14,3 +14,4 @@ ALTER TABLE files ADD COLUMN blockchain_hash    VARCHAR;
 ALTER TABLE files ADD COLUMN exported_timestamp INTEGER;
 
 ALTER TABLE transfer_meter ADD COLUMN month VARCHAR;
+CREATE UNIQUE INDEX transfer_meter_month_idx ON transfer_meter(month);
