260c3348769c0116c5d47b452ccc09eaeae30cee
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-07-11 02:05:35 +0100

    Changes database connection string to a connection uri

--- cloudmanager/database.py
+++ cloudmanager/database.py
@@ -1,9 +1,23 @@
 import sqlite3
 import psycopg2
 import psycopg2.extras
+import urlparse
 
-def connect(*args, **kwargs):
-    return connect_psycopg2(*args, **kwargs)
+def connect(uri):
+    result = urlparse.urlparse(uri)
+    username = result.username
+    password = result.password
+    database = result.path[1:]
+    hostname = result.hostname
+
+    if result.scheme == 'postgres' or result.scheme == 'postgresql':
+        return connect_psycopg2(
+                database=database,
+                user=username,
+                password=password,
+                host=hostname)
+
+    return None
 
 def connect_sqlite3(*args, **kwargs):
     db = sqlite3.connect(*args, **kwargs)
