bd285a7beb2852bc254b7192db617560f65f5446
john5223 <curran736@gmail.com>
2014-09-08 16:21:22 -0500

    fixed tabs/spaces

--- tests/test_setup_db.py
+++ tests/test_setup_db.py
@@ -2,11 +2,10 @@ import os
 from cloudmanager.setup_db import setup_db
 
 def test_setup_db():
-	try:
-		setup_db('test.db')
-		assert 1 == 1
-	except Exception as e:
-		raise e
-	finally:
-		os.remove('test.db')
-	
+    try:
+        setup_db('test.db')
+        assert 1 == 1
+    except Exception as e:
+        raise e
+    finally:
+        os.remove('test.db')
