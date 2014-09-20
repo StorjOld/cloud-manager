a063968aa4b60c9e511de5d62c0a02648210da87
brian <androm3da>
2014-09-20 16:10:03 -0500

    Added more test assertions to test_cloudmanager

--- tests/postgres/test_cloudmanager.py
+++ tests/postgres/test_cloudmanager.py
@@ -1,5 +1,6 @@
 import cloudmanager
 from settings import TEST_DB_PATH
+import os
 
 def test_cloudmanager():
 
@@ -11,6 +12,9 @@ def test_cloudmanager():
         upload2 = cm.upload("files/test.mp3")
         assert upload2
 
+        result = cm.download("invalidhash")
+        assert not result
+
         assert cm.exists(upload1)
         assert cm.on_cache(upload1)
         assert cm.usage_ratio()
@@ -18,6 +22,22 @@ def test_cloudmanager():
         assert cm.warm_up(upload1)
         assert cm.warm_up(upload2)
 
+        cm.cloud_sync()
+
         data = cm.data_dump(128000)
-        #print data
-        #print cm.data_load(data, "blockchain-hash")
+        lkb = cm.last_known_block()
+        assert lkb == 0
+        genesis_block = cm.visit_block(lkb)
+        assert not genesis_block
+
+        assert cm.exists(upload2)
+        uploadinfo = cm.info(upload2)
+        assert uploadinfo['filesize'] == os.stat('files/test.mp3').st_size
+
+        load_success = cm.data_load(data, upload2)
+        assert load_success
+
+        assert cm.total_incoming() == 0
+        assert cm.total_outgoing() == 0
+        assert cm.current_incoming() == 0
+        assert cm.current_outgoing() == 0
